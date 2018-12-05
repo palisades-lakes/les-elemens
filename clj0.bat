d51dd12c0a360ad165d9c93dd27dd938c7c26476
John McDonald <palisades.lakes@gmail.com>
2018-12-05 10:01:44 -0800

    obsolete clj launchers

--- clj0.bat
+++
@@ -1,7 +0,0 @@
-::call "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\mkl\bin\mklvars.bat" intel64
-@echo off
-set XMX=-Xms28g -Xmx28g -Xmn11g 
-set CP=-cp ./src/scripts/clojure;lib/*
-set JAVA="%JAVA_HOME%\bin\java"
-set CMD=%JAVA% -ea %XMX% %CP% clojure.main %*
-%CMD%
