583672515936eab453a43f719cdd9cdbebdbe73b
John McDonald <palisades.lakes@gmail.com>
2018-12-05 15:21:26 -0800

    algebra implementation start

---
+++ src/scripts/c/numbers.c
@@ -0,0 +1,23 @@
+#include <math.h>
+#include <limits.h>
+#include <float.h>
+#include <stdint.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+//--------------------------------------------------------
+
+int main (void) {
+
+  printf("\nunsigned int:\n");
+
+  unsigned int u0 = 0xFFFFFFFF;
+  printf("u0 = %u\n",u0);
+  printf("u0 + 1 = %u\n",u0+1);
+  printf("u0 - 1 = %u\n",u0-1);
+
+  }
+
+//--------------------------------------------------------
---
+++ src/scripts/c/numbers2.c
@@ -0,0 +1,22 @@
+#include <limits.h>
+#include <stdint.h>
+#include <float.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+//--------------------------------------------------------
+
+int main (void) {
+
+
+  printf("\nunsigned int:\n");
+
+  unsigned int u0 = 0xFFFFFFFF;
+  printf("u0 = %d\n",u0);
+  printf("u0 +1 = %d\n",u0+1);
+
+  }
+
+//--------------------------------------------------------
