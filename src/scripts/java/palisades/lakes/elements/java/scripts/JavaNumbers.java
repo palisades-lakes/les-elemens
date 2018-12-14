5446af8483d9d0b2384fb4cde52d774125771b4b
John Alan McDonald <palisades.lakes@gmail.com>
2018-12-13 16:35:20 -0800

    more numbers

--- src/scripts/java/palisades/lakes/elements/java/scripts/JavaNumbers.java
+++ src/scripts/java/palisades/lakes/elements/java/scripts/JavaNumbers.java
@@ -6,7 +6,7 @@ package palisades.lakes.elements.java.scripts;
  * </pre>
  *
  * @author palisades dot lakes at gmail dot com
- * @version 2018-12-12
+ * @version 2018-12-13
  */
 
 @SuppressWarnings("unchecked")
@@ -14,19 +14,65 @@ public final class JavaNumbers {
 
   //--------------------------------------------------------------
 
+  private static final void add (final double a,
+                                 final double b) {
+    System.out.println(
+      String.format("%f + %f = %f",
+        Double.valueOf(a),
+        Double.valueOf(b),
+        Double.valueOf(a+b))); }
+
+  private static final void subtract (final double a,
+                                      final double b) {
+    System.out.println(
+      String.format("%f - %f = %f",
+        Double.valueOf(a),
+        Double.valueOf(b),
+        Double.valueOf(a-b))); }
+
+  private static final void multiply (final double a,
+                                      final double b) {
+    System.out.println(
+      String.format("%f * %f = %f",
+        Double.valueOf(a),
+        Double.valueOf(b),
+        Double.valueOf(a*b))); }
+
+  private static final void divide (final double a,
+                                    final double b) {
+    System.out.println(
+      String.format("%f / %f = %f",
+        Double.valueOf(a),
+        Double.valueOf(b),
+        Double.valueOf(a/b))); }
+
+  private static final void combinations (final double a,
+                                    final double b) {
+    add(a,b);
+    subtract(a,b);
+    multiply(a,b);
+    divide(a,b); }
+
+  //--------------------------------------------------------------
+
   public static final void main (final String[] args) {
 
-//    System.out.println("i =" + Integer.MAX_VALUE); 
-//    System.out.println("i + 1 =" + (Integer.MAX_VALUE + 1)); 
-    
-    byte b = Byte.MIN_VALUE;
-    for (int i = 0; i < 3*Byte.MAX_VALUE; i++) {
-      System.out.println(
-        String.format("%d : %d %x",
-          Integer.valueOf(i),
-          Byte.valueOf(b),
-          Byte.valueOf(b))); 
-      b++;}
+    combinations(Double.POSITIVE_INFINITY,Double.NEGATIVE_INFINITY);
+    combinations(1.0,Double.POSITIVE_INFINITY);
+    combinations(0.0,Double.POSITIVE_INFINITY);
+    combinations(0.0,-0.0);
+       //    System.out.println("i =" + Integer.MAX_VALUE); 
+    //    System.out.println("i + 1 =" + (Integer.MAX_VALUE + 1)); 
+
+    //    byte b = Byte.MIN_VALUE;
+    //    for (int i = 0; i < 3*Byte.MAX_VALUE; i++) {
+    //      System.out.println(
+    //        String.format("%d : %d %x",
+    //          Integer.valueOf(i),
+    //          Byte.valueOf(b),
+    //          Byte.valueOf(b))); 
+    //      b++;}
+
   }
 
   //--------------------------------------------------------------
